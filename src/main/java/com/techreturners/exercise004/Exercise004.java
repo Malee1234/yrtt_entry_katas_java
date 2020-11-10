be824d2d68641d05b10901489b97ad5e2fd2a08c
Malee1234 <mpinkis@hotmail.com>
2020-11-10 23:18:49 +0000

    Comitted

--- src/main/java/com/techreturners/exercise004/Exercise004.java
+++ src/main/java/com/techreturners/exercise004/Exercise004.java
@@ -1,12 +1,16 @@
 package com.techreturners.exercise004;
+import javax.swing.*;
+import java.String.*;
 
 public class Exercise004 {
 
      // Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
     // Leave punctuation marks untouched.
 
-    public String pigLatin(String str) {
-        // Add your code here
-        return "";
+     public String pigLatin(String str) {
+        char firstLetter = str.charAt(0); //get the first letter
+        str = str.substring(1); //remove the first letter from the input string
+        str = str + firstLetter + "ay"; //add first letter and "ay" to end of input string
+        return str;
     }
 }
