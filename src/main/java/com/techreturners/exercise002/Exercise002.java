e4c9019b87aec54db0c41176d1abb0af0a6e80c5
Malee1234 <mpinkis@hotmail.com>
2020-11-10 23:04:59 +0000

    Update Exercise002.java

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -1,4 +1,6 @@
 package com.techreturners.exercise002;
+import java.util.Calendar;
+import java.lang.*;
 
 public class Exercise002 {
 
@@ -7,6 +9,33 @@ public class Exercise002 {
 
     public int past(int h, int m, int s) {
         // Your code here!
-        return 0;
+        long timeinmilli=0;
+        long midnightimeinmilli=0;
+
+        Calendar c1 = Calendar.getInstance();
+        Calendar midnite = Calendar.getInstance();
+    
+        midnite.add(Calendar.DAY_OF_MONTH, 1);
+        midnite.set(Calendar.HOUR_OF_DAY, 0);
+        midnite.set(Calendar.MINUTE, 0);
+        midnite.set(Calendar.SECOND, 0);
+        midnite.set(Calendar.MILLISECOND, 0);
+        midnightimeinmilli=midnite.getTimeInMillis();
+       
+        c1.add(Calendar.DAY_OF_MONTH, 1);
+        c1.set(Calendar.MINUTE, m);
+        c1.set(Calendar.HOUR, h);
+        c1.set(Calendar.SECOND, s);
+        timeinmilli=c1.getTimeInMillis();   
+        
+        if(timeinmilli > midnightimeinmilli)
+            return timeinmilli;
+        else 
+         return 0;
+    }
+
+    public static void main(String args[]) 
+    { 
+        System.out.println("midnite" + past(2,30,0)); 
     }
 }
